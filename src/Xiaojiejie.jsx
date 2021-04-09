2ae0a92be16850c7712189762164ae3bc8f6021b
zeguo <61856352+zg-zhang@users.noreply.github.com>
2021-04-09 19:07:48 +0800

    08 数据绑定

--- src/Xiaojiejie.jsx
+++ src/Xiaojiejie.jsx
@@ -1,16 +1,31 @@
 import React, {Component, Fragment} from "react";
 
 class Xiaojiejie extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            inputValue: 'zeguo',
+            list: []
+        }
+    }
+
     render() {
         return (
             <Fragment>
-                <div><input/><button> 增加服务 </button></div>
+                <div><input value={this.state.inputValue} onChange={this.inputChange.bind(this)}/><button> 增加服务 </button></div>
                 <ul>
                     <li>头部按摩</li>
                     <li>精油推背</li>
                 </ul>
             </Fragment>
         );
+    }
+
+    inputChange(e) {
+        console.log(e.target.value)
+        this.setState({
+            inputValue: e.target.value
+        })
     }
 }
 
