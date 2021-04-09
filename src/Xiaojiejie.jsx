70dff6276f3458c9d1e2254578feaa2228017cfa
zeguo <61856352+zg-zhang@users.noreply.github.com>
2021-04-09 19:46:14 +0800

    13 组件拆分

--- src/Xiaojiejie.jsx
+++ src/Xiaojiejie.jsx
@@ -1,10 +1,11 @@
 import React, {Component, Fragment} from "react";
+import XiaojiejieItem from './XiaojiejieItem'
 
 class Xiaojiejie extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            inputValue: 'zeguo',
+            inputValue: '',
             list: ['基础按摩', '精油按摩']
         }
     }
@@ -20,12 +21,7 @@ class Xiaojiejie extends Component {
                     {
                         this.state.list.map((item, index) => {
                             return (
-                                <li
-                                    key={index + item}
-                                    onClick={this.deleteItem.bind(this, index)}
-                                >
-                                    {item}
-                                </li>
+                               <XiaojiejieItem key={index + item}/>
                             )
                         })
                     }
---
+++ src/XiaojiejieItem.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class XiaojiejieItem extends Component {
+    render() {
+        return (
+            <div>
+                小姐姐
+            </div>
+        );
+    }
+}
+
+export default XiaojiejieItem
