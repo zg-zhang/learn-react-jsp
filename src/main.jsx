a85fe5b77344c3b4de2ecd8dacebe48a8752fd18
zeguo <61856352+zg-zhang@users.noreply.github.com>
2021-04-09 18:57:14 +0800

    07 小姐姐菜单

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import Xiaojiejie from "./Xiaojiejie";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Xiaojiejie />
   </React.StrictMode>,
   document.getElementById('root')
 )
