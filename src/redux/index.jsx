09321a5740ada123a670d33eb6b5d6ae5bc2d5dd
zeguo <61856352+zg-zhang@users.noreply.github.com>
2021-04-10 18:34:35 +0800

    34 redux 增加redux

--- src/redux/index.jsx
+++ src/redux/index.jsx
@@ -1,12 +1,10 @@
 import React, { useState } from 'react'
 import {Button, Input, List} from 'antd'
+import store from "./store";
 
 function Redux() {
-    const [data, setData] = useState([
-        '早8点开晨会，分配今天的开发工作',
-        '早9点和项目经理作开发需求讨论会',
-        '晚5:30对今日代码进行review'
-    ])
+    const state = store.getState()
+    const [data, setData] = useState(state.list)
 
     return (
         <div>
---
+++ src/redux/store/reducer.js
@@ -0,0 +1,11 @@
+const defaultState = {
+    inputValue: 'write someThing',
+    list: [
+        '早上4点起床，锻炼身体',
+        '中午下班游泳一小时'
+    ]
+}
+
+export default (state = defaultState, action) => {
+    return state
+}
