6b8d69971e3af5fe35f967c01eb6249775e2636d
zeguo <61856352+zg-zhang@users.noreply.github.com>
2021-04-10 18:59:09 +0800

    35 redux 新增新增方法

--- src/redux/index.jsx
+++ src/redux/index.jsx
@@ -3,19 +3,48 @@ import {Button, Input, List} from 'antd'
 import store from "./store";
 
 function Redux() {
-    const state = store.getState()
-    const [data, setData] = useState(state.list)
+    const [data, setData] = useState(store.getState())
+
+    function handleChange(e) {
+        console.log(e.target.value)
+        // 创建 Action
+        const action = {
+            type: 'change_input_value',
+            value: e.target.value
+        }
+        store.dispatch(action)
+    }
+
+    function handleClick() {
+        const action = {
+            type: 'add_click'
+        }
+        store.dispatch(action)
+    }
+
+    function handleStoreChange() {
+        setData(store.getState())
+    }
+
+    store.subscribe(handleStoreChange)
 
     return (
         <div>
             <div>
-                <Input placeholder='zeguo' style={{ width: '250px', margin: '10px' }} />
-                <Button type='primary' >增加</Button>
+                <Input
+                    style={{ width: '250px', margin: '10px' }}
+                    value={data.inputValue}
+                    onChange={handleChange}
+                />
+                <Button
+                    type='primary'
+                    onClick={handleClick}
+                >增加</Button>
             </div>
             <div style={{ margin: '10px', width: '300px' }}>
                 <List
                     bordered
-                    dataSource={data}
+                    dataSource={data.list}
                     renderItem={item => (<List.Item>{item}</List.Item>)}
                 />
             </div>
--- src/redux/store/reducer.js
+++ src/redux/store/reducer.js
@@ -7,5 +7,16 @@ const defaultState = {
 }
 
 export default (state = defaultState, action) => {
+    console.log(state, action)
+    if (action.type === 'change_input_value') {
+        let newState = JSON.parse(JSON.stringify(state))
+        newState.inputValue = action.value
+        return  newState
+    } else if (action.type === 'add_click') {
+        let newState = JSON.parse(JSON.stringify(state))
+        newState.list.push(newState.inputValue)
+        newState.inputValue = ''
+        return  newState
+    }
     return state
 }
