1e3aa959e55d39b0c14a6942a8bc735bc737ec6a
zeguo <61856352+zg-zhang@users.noreply.github.com>
2021-04-10 10:40:58 +0800

    目录变动

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-import Xiaojiejie from "./Xiaojiejie";
+import Xiaojiejie from "./react/Xiaojiejie";
 
 ReactDOM.render(
   <React.StrictMode>
