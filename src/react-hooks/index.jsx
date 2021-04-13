137299d8ba6080ed3c0a034fa9a23d57ba92b040
zeguo <61856352+zg-zhang@users.noreply.github.com>
2021-04-13 23:38:19 +0800

    61 useContext

--- src/react-hooks/index.jsx
+++ src/react-hooks/index.jsx
@@ -1,17 +1,31 @@
-import React, {useEffect, useState} from 'react'
+import React, {createContext, useState, useContext} from 'react'
+import {Button} from "antd";
+
+const CountContext = createContext()
 
 function ReactHooks() {
     const [num, setNum] = useState(0)
 
-    useEffect(() => {
-        console.log(`useEffect => You clicked ${num} times`)
-    })
+    function Index(props) {
+        return <h2>Index {props.children}</h2>
+    }
+
+    function List() {
+        const num = useContext(CountContext)
+        return <h2>list: {num}</h2>
+    }
 
     return (
         <div>
-            <div onClick={() => {setNum(num - 1)}}>-</div>
+            <Button onClick={() => {setNum(num + 1)}}> + </Button>
             <div>{num}</div>
-            <div onClick={() => {setNum(num + 1)}}>+</div>
+            <Button onClick={() => {setNum(num - 1)}}> - </Button>
+
+            <CountContext.Provider value={num}>
+                <Index>
+                    <List />
+                </Index>
+            </CountContext.Provider>
         </div>
     )
 }
